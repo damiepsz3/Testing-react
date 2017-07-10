326dc9717917656b0d11c4205b4c04bc411b102b
damiepsz3 <personal@AFS-ML-105.home>
2017-07-10 01:07:43 -0400

    Creating components dinamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import Hello from './components/Hello';
-import Buttonify from './components/Buttonify';
+import Circle from './components/Circle';
 import './App.css';
 
 class App extends Component {
+
+
+
   render() {
+    var theCircles = <Circle bgColor="#F9C240"/>
+    var showCircle = () => {
+      var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363"];
+      var ran = Math.floor(Math.random() * colors.length);
+      return <Circle bgColor={colors[ran]}/>;
+    }
+    var colors = ["#393E41", "#E94F37", "#1C89BF", "#A1D363",
+                  "#85FFC7", "#297373", "#FF8552", "#A40E4C"];
+    var colorsCircle = (colors) => {
+      var items = [];
+      for(var i=0; i < colors.length; i++) {
+        var color = colors[i];
+        items.push(<Circle key={i + color} bgColor={color}/>)
+      }
+      return items;
+    }
+
     return (
       <div className="App">
-        <Hello greetTarget="Pichon de picuru"/>
-        <Buttonify behavior="Submit">SEND DATA</Buttonify>
+         {colorsCircle(colors)}
       </div>
     );
   }
---
+++ src/components/Circle.js
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react'
+
+class Circle extends React.Component {
+  render () {
+    var circleStyle = {
+      padding: 10,
+      margin: 20,
+      display: "inline-block",
+      backgroundColor: this.props.bgColor,
+      borderRadius: "50%",
+      width: 100,
+      height: 100
+    };
+    return (
+      <div style={circleStyle}>
+
+      </div>
+    );
+  }
+}
+
+export default Circle;
